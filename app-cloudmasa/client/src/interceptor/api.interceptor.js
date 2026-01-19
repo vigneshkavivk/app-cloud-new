78b79d3115736fb1e6fc025e2599429cffdaaa25
vigneshkavivk <vigneshkavivk@gmail.com>
2026-01-19 16:13:39 +0530

    Update api.interceptor.js

--- app-cloudmasa/client/src/interceptor/api.interceptor.js
+++ app-cloudmasa/client/src/interceptor/api.interceptor.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: '/',
   headers: {
     'Content-Type': 'application/json', // ✅ critical for POST body
     Accept: 'application/json',
