b1d806961f9dc8618b9455cd36dbef917447b571
vigneshkavivk <vigneshkavivk@gmail.com>
2026-02-02 11:11:37 +0530

    Delete terraform-azure directory

--- terraform-azure/modules/vm-ware/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "vm_public_ip" {
-  value = azurerm_linux_virtual_machine.main.public_ip_address
-}
--- terraform-azure/modules/vnet/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "vnet_name" {
-  value = azurerm_virtual_network.main.name
-}
-
-output "subnet_ids" {
-  value = { for k, v in azurerm_subnet.subnets : k => v.id }
-}
